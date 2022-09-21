67b379118963428e0baa07691d9e99a9e20dccc3
Amit Kumar <amit_kumar2@anfcorp.com>
2022-09-21 18:40:09 -0500

    first

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Landing
+ from "./pages/Landing";
+function App() {
+  return (
+    <div>
+       <h1>Jobify</h1>
+       <Landing></Landing>
+    </div>
+  
+  );
+}
+
+export default App;
