2be47877bbe9f34b93ccd86b4d77642ee079396a
Amit Kumar <amit_kumar2@anfcorp.com>
2022-09-21 19:48:49 -0500

    file add

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
-import Landing
- from "./pages/Landing";
+import Landing from "./pages/Landing";
+import styled from 'styled-components';
+
+const Button = styled.button`
+background: red;
+color: white;
+font-size:1rem; 
+`
+
 function App() {
   return (
     <div>
        <h1>Jobify</h1>
        <Landing></Landing>
     </div>
-  
   );
 }
 
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,7 +4,20 @@ import main from '../assets/images/main.svg';
 
 const Landing = () => {
   return (
-    <h4>Landing Page</h4>
+    <main>
+      <nav>
+        <img src={logo} alt="logo" className='logo '/>
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>Job <span>tracking</span>app</h1>
+          <p>This is details</p>
+          <button className='btn btn-hero'> Login / Register </button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </main>
   )
 }
 
